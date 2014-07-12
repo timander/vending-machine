5ddaea049f2ff7de6e32907627f35e72545ea69a
Tim Andersen <timander@gmail.com>
2014-07-12 18:30:11 -0500

    added second test, forcing removal of fake implementatiton

--- src/main/java/net/timandersen/VendingMachine.java
+++ src/main/java/net/timandersen/VendingMachine.java
@@ -1,19 +1,22 @@
 package net.timandersen;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachine {
 
+    Map<String, Integer> inventory = new HashMap<String, Integer>();
 
-
-    public void releaseProduct(String itemCode) {
-
+    public void releaseProduct(String productCode) {
+        Integer currentQuantity = getQuantityFor(productCode);
+        inventory.put(productCode, currentQuantity - 1);
     }
-
 
     public void addProducts(Product product, int quantity) {
-
+        inventory.put(product.getCode(), quantity);
     }
 
-    public int getQuantityFor(String code) {
-        return 19;
+    public int getQuantityFor(String productCode) {
+        return inventory.get(productCode);
     }
 }
--- src/test/java/net/timandersen/VendingMachineTest.java
+++ src/test/java/net/timandersen/VendingMachineTest.java
@@ -8,10 +8,18 @@ public class VendingMachineTest {
     @org.junit.Test
     public void trackProductQuantity() {
         VendingMachine vendingMachine = new VendingMachine();
-        Product product = new Product("A1", "Snickers");
-        vendingMachine.addProducts(product, 20);
-        vendingMachine.releaseProduct("A1");
-        assertEquals(19, vendingMachine.getQuantityFor(product.getCode()));
+
+        Product snickers = new Product("A1", "Snickers");
+        Product mms = new Product("A2", "M&Ms");
+
+        vendingMachine.addProducts(snickers, 20);
+        vendingMachine.addProducts(mms, 15);
+
+        vendingMachine.releaseProduct(snickers.getCode());
+        vendingMachine.releaseProduct(mms.getCode());
+
+        assertEquals(19, vendingMachine.getQuantityFor(snickers.getCode()));
+        assertEquals(14, vendingMachine.getQuantityFor(mms.getCode()));
     }
 
 }
